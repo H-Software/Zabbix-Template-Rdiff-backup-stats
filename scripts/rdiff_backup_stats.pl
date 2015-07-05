1e4c9414de3ca12d35f39069de40efe7e237a5ff
Patrik Majer <patrik.majer@atlas.cz>
2015-07-05 20:36:46 +0200

    rewrite for zabbix-agent usage, cleaning code

--- scripts/rdiff_backup_stats.pl
+++ scripts/rdiff_backup_stats.pl
@@ -38,12 +38,6 @@ my @date;
 my @mir_list;
 my $repository;
 my %opts=();
-my $w_thresh;
-my $c_thresh;
-my $s_thresh;
-my $t_thresh;
-my $l_thresh;
-my $cron_cycle;
 my $time_stamp;
 my $no_mir;
 my $stats_fn;
@@ -54,20 +48,15 @@ my $pid_1;
 my $pid_2;
 my $size_now;
 my $size_change;
-my $script;
-my $err = 0;
-#r=repository
-#p=cron cylce;
-#w=warning threshold
-#c=critical threshold
-#l=changed size too large
-#t=hour of day becomes critical
-#h=bool help
-#s=low sourcesize critical
+my $debug;
+#my $err = 0;
+my $command = $ARGV[2];
+my $result;
 
 if($<)
 {
-	print "ERROR: Must run as root\n";
+	$debug .= "ERROR: Must run as root\n";
+	print "0\n";
 	exit(3);
 }
 
@@ -79,48 +68,12 @@ if (!getopts( "r:w:c:l:p:s:e:", \%opts ))
   exit 1;
 }
 
-#getopts( "r:w:c:l:p:s:e:", \%opts ) or usage();
-#
-#for(qw{r w c l p})
-#{	
-#	if(!exists $opts{$_})
-#	{
-#		print "Arguement -$_ is not optional\n";
-#		$err = 1;
-#	}
-#}
-
-#if($err)
-#{
-#	print "\n";
-#	usage ();
-#}
-
 $repository = "$opts{r}/rdiff-backup-data";
-$w_thresh = $opts{w};
-$c_thresh = $opts{c};
-#$l_thresh = $opts{l} * 1048576;
-$t_thresh = $opts{t};
-#$cron_cycle = $opts{p} * 3600;
-#$script = $opts{e};
 
-if(!-d$repository)
-{
-	print "Invalid repository directory\n";
-	exit(3);
+if($command eq ""){
+	print "ERROR: Invalid \"COMMAND\"\n";
+	exit(6);
 }
-
-#if(defined $script)
-#{
-#	if(system($script) != 0)
-#	{
-#		print "CRITICAL: $script exited abnormally\n";
-#		exit(2);
-#	}
-#}
-
-$s_thresh = $opts{s} * 1048576 if(defined($opts{s}));
-$s_thresh = 1048576 if(!defined($opts{s}));
 
 @mir_list = <$repository/current_mirror*>;
 $no_mir = scalar @mir_list;
@@ -144,26 +97,10 @@ if($no_mir == 1)
 	print stat($cur_mir)->mtime;
 	print "\n";
 	
-#	if($elapsed > 20)
-#	{
-#		if ($elapsed < 3600)
-#		{
-#			printf("CRITICAL: Backup failed to run (%.1f minutes overdue) Last backup: %s\n",
-#		  	$elapsed / 60,
-#			scalar(localtime(stat($stats_fn)->mtime)));
-#		}
-#		else
-#		{
-#			printf("CRITICAL: Backup failed to run (%.1f hours overdue) Last backup: %s\n",
-#		  	$elapsed / 3600,
-#			scalar(localtime(stat($stats_fn)->mtime)));
-#		}		
-#		exit(2);
-#	}
-	
 	if(!open(FILE, "< $stats_fn"))
 	{
-		print "ERROR: Could not open stat file\n";
+		$debug .= "ERROR: Could not open stat file\n";
+		print "0\n";
 		exit(3);
 	}
 	
@@ -175,14 +112,7 @@ if($no_mir == 1)
 	print "Total size...(in MB)\n";
 	print $size_now;
 	print "\n";
-	
-	#if($size_now <= $s_thresh)
-	#{
-	#	$s_thresh /= 1048576;
-	#	print "CRITICAL: Source repository size dropped under $s_thresh megabyte(s)\n";
-	#	exit(2);			
-	#}
-	
+
 	<FILE>;<FILE>;<FILE>;<FILE>;<FILE>;<FILE>;<FILE>;<FILE>;<FILE>;<FILE>;<FILE>;
 	($size_change) = <FILE> =~ /TotalDestinationSizeChange (.*) \(.*\)$/;
 	
@@ -190,15 +120,23 @@ if($no_mir == 1)
 	print "Change size...(in MB)\n";
 	print $size_change;
 	print "\n";
-	
-	#if($size_change >= $l_thresh)
-	#{
-	#	$size_change /= 1048576;
-	#	printf("WARNING: Transferred more than threshold(%dMB)\n", $size_change);
-	#	exit(1);
-	#}
+
 	$elapsed = localtime(stat($stats_fn)->mtime);
-	printf("OK: Last backup finished %s. Size change %d MB\n", $elapsed, $size_change);
+	
+	$debug .= "OK: Last backup finished ".$elapsed.". Size change ". $size_change." MB\n";
+	$result = "1";
+	
+	if($command eq "status"){
+	    print $result . "\n";
+	}
+	else{
+	    print "ERROR: Unknown mode! \n";
+	}
+	
+	if($opts{d}){
+	    print $debug;
+	}
+	
 	exit(0);
 }
 
@@ -253,7 +191,8 @@ if($no_mir == 2)
 	exit(2);
 }
 
-print "ERROR: Neither one current mirror, nor two";
+$debug .= "ERROR: Neither one current mirror, nor two";
+print "0\n";
 exit(3);
 
 sub running()
@@ -261,21 +200,21 @@ sub running()
 	$cur_mir = <$repository/current_mirror*>;
 	($time_stamp) = ($mir_list[0] =~ /current_mirror\.(.*)\.data$/);
 	$stats_fn = "$repository/session_statistics.$time_stamp.data";
-	$elapsed = ((time() - $cron_cycle) - stat($stats_fn)->mtime);
-	if($elapsed > 0)
-	{
-		@hour = localtime(time());
-		if($hour[2] >= $c_thresh)
-		{
-			print "CRITICAL: Backup still running after $c_thresh:00\n";
-			exit(2);
-		}
-		if($hour[2] >= $w_thresh)
-		{
-			print "WARNING: Backup running after $w_thresh:00\n";
-			exit(1);
-		}
-	}
+#	$elapsed = ((time() - $cron_cycle) - stat($stats_fn)->mtime);
+#	if($elapsed > 0)
+#	{
+#		@hour = localtime(time());
+#		if($hour[2] >= $c_thresh)
+#		{
+#			print "CRITICAL: Backup still running after $c_thresh:00\n";
+#			exit(2);
+#		}
+#		if($hour[2] >= $w_thresh)
+#		{
+#			print "WARNING: Backup running after $w_thresh:00\n";
+#			exit(1);
+#		}
+#	}
 	
 	print "OK: Backup in progress\n";
 	exit(0);
@@ -283,15 +222,14 @@ sub running()
 
 sub usage()
 {
-	print "Usage: rdiff_backup_stats.pl [OPTIONS]\n
+	print "Usage: rdiff_backup_stats.pl [OPTIONS] [COMMAND]\n
+      OPTIONS:
 	-r <rdiff repository>
-	-w <time warning threshold>
-	-c <time critical threshold>
-	-l <transferred warning threshold>
-	-p <cron period>\n
-	-s <optional small size threshold>
-	-e <optional script to execute>\n";
+	-d <debug>\n
+      COMMAND:
+	status <status of backup>
+	ct <change size>
+	ts <total size"
+	.">\n";
 	exit(3);
 }
-
-
